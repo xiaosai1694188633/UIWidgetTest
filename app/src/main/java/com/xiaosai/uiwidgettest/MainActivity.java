88b68784e4c169c7b11b0686b7765e8444324317
han <1694188633@qq.com>
2016-03-01 11:18:08 +0800

    第一次提交

---
+++ app/src/main/java/com/xiaosai/uiwidgettest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.xiaosai.uiwidgettest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/3/1 0001.
+ */
+public class MainActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
